65f9fecbf8fed3e815ecdac85bfa372b7ed32414
Kelly Lougheed <kellylougheed@gmail.com>
2016-11-15 20:33:40 -0800

    Rewrite app in AngularJS

---
+++ app.js
@@ -0,0 +1,114 @@
+(function() {
+  'use strict';
+
+  angular.module('RomanNumeral', [])
+
+  .controller('RomanNumeralController', RomanNumeralController);
+
+  RomanNumeralController.$inject = ['$scope'];
+
+  function RomanNumeralController($scope) {
+
+    $scope.arabicNumber = Math.floor((Math.random() * 1000) + 1);
+    $scope.answer = "";
+    $scope.verdict = "";
+    $scope.correctAnswer = "";
+
+    $scope.newNumber = function() {
+      $scope.verdict = "";
+      $scope.correctAnswer = "";
+      $scope.answer = "";
+      $scope.arabicNumber = Math.floor((Math.random() * 1000) + 1);
+    }
+
+    function convertToRoman(num) {
+      var romanNumeral = "";
+      num = Math.floor(num);
+      var number = num.toString();
+      // To convert the thousand's place
+      if (number.length == 4) {
+        for (var i = number.charAt(0); i > 0; i--) {
+          romanNumeral += "M";
+        }
+      }
+      // To convert the hundred's place
+      if (number.length >= 3) {
+        var x = number.charAt(number.length - 3);
+        if (x >= 5) {
+          if (x == 9) {
+            romanNumeral += "CM";
+          } else {
+            romanNumeral += "D"; 
+            if (x > 5) {
+              for (x; x > 5; x--) {
+                romanNumeral += "C";
+              }
+            }
+          } 
+        } else if (x == 4) {
+          romanNumeral += "CD";
+        } else if (x < 4) {
+          for (x; x > 0; x--) {
+            romanNumeral += "C";
+          }
+        }
+      }
+      // To convert the ten's place
+      if (number.length >= 2) {
+        var y = number.charAt(number.length - 2);
+        if (y >= 5) {
+          if (y == 9) {
+            romanNumeral += "XC";
+          } else {
+            romanNumeral += "L";
+            if (y > 5) {
+              for (y; y > 5; y--) {
+                romanNumeral += "X";
+              }
+            }
+          }
+        } else if (y == 4) {
+          romanNumeral += "XL";
+        } else if (y < 4) {
+          for (y; y > 0; y--) {
+            romanNumeral += "X";
+          }
+        }
+      }
+      // To convert the one's place
+      var z = number.charAt(number.length - 1);
+      if (z >= 5) {
+        if (z == 9) {
+          romanNumeral += "IX";
+        } else {
+          romanNumeral += "V"; 
+          if (z > 5) {
+            for (z; z > 5; z--) {
+              romanNumeral += "I";
+            }
+          }
+        } 
+      } else if (z == 4) {
+        romanNumeral += "IV";
+      } else if (z < 4) {
+        for (z; z > 0; z--) {
+          romanNumeral += "I";
+        }
+      }
+     return romanNumeral;
+    }
+
+    $scope.checkAnswer = function(arabicNumber, answer) {
+        var correctAnswer = convertToRoman(arabicNumber);
+        answer = answer.toUpperCase();
+        if (correctAnswer == answer) {
+          $scope.verdict = "Correct!";
+        } else if (correctAnswer != answer) {
+          $scope.verdict = "Incorrect!";
+          $scope.correctAnswer = "The correct answer was "+correctAnswer+".";
+        }
+    }
+
+  }
+
+})();
